2996625b49d0b0ec84ee127aad38ad5b12f38332
jaennil <jaennil@yandex.ru>
2024-07-18 21:26:00 +0300

    feat: install package

--- src/tui.rs
+++ src/tui.rs
@@ -1,18 +1,16 @@
-use std::io::{self, stdout, Stdout};
+use std::io::{self, stdout};
 
 use ratatui::{
-    backend::CrosstermBackend,
-    crossterm::{
+    backend::CrosstermBackend, crossterm::{
         event::{DisableMouseCapture, EnableMouseCapture},
         execute,
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    },
-    Terminal,
+    }
 };
 
-pub type Tui = Terminal<CrosstermBackend<Stdout>>;
+pub type Terminal = ratatui::Terminal<ratatui::backend::CrosstermBackend<std::io::Stdout>>;
 
-pub fn init() -> io::Result<Tui> {
+pub fn init() -> io::Result<Terminal> {
     execute!(stdout(), EnterAlternateScreen, EnableMouseCapture)?;
     enable_raw_mode()?;
     Terminal::new(CrosstermBackend::new(stdout()))
