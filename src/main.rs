10a10b3edad9e79f2e1ee928daa76747beac0e75
jaennil <jaennil@yandex.ru>
2024-07-18 12:57:10 +0300

    feat: select table items

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::io::{self};
 
 use ratatui::{
-    buffer::Buffer,
-    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Constraint, Layout, Rect},
-    style::{Color, Style, Stylize},
-    widgets::{Block, Paragraph, Row, Table, Widget},
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style, Stylize},
+    widgets::{Block, Paragraph, Row, Table, TableState},
     Frame,
 };
 
@@ -16,10 +15,17 @@ use pacman::{Package, Pacman};
 
 fn main() -> io::Result<()> {
     let mut terminal = tui::init()?;
-    let mut app = App::default();
+    let mut app = App::new();
     let result = app.run(&mut terminal);
     tui::restore()?;
     result
+}
+
+#[derive(Default)]
+enum Mode {
+    #[default]
+    Search,
+    Table,
 }
 
 #[derive(Default)]
@@ -27,10 +33,19 @@ struct App {
     pacman: Pacman,
     search: String,
     packages: Vec<Package>,
+    table_state: TableState,
+    mode: Mode,
     exit: bool,
 }
 
 impl App {
+    fn new() -> Self {
+        Self {
+            table_state: TableState::default().with_selected(0),
+            ..Default::default()
+        }
+    }
+
     fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
@@ -39,46 +54,12 @@ impl App {
         Ok(())
     }
 
-    fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Esc => self.exit(),
-            KeyCode::Backspace => {
-                self.search.pop();
-                self.packages = self.pacman.search(&self.search);
-            }
-            KeyCode::Char(value) => {
-                self.search.push(value);
-                self.packages = self.pacman.search(&self.search);
-            }
-            _ => {}
-        }
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let layout = Layout::vertical([Constraint::Length(3), Constraint::Min(3)]).split(area);
+    fn render_frame(&mut self, frame: &mut Frame) {
+        let layout =
+            Layout::vertical([Constraint::Length(3), Constraint::Min(3)]).split(frame.size());
 
         let search = Paragraph::new(self.search.clone()).block(Block::bordered());
-        search.render(layout[0], buf);
+        frame.render_widget(search, layout[0]);
 
         let mut rows = Vec::new();
         for package in &self.packages {
@@ -94,7 +75,87 @@ impl Widget for &App {
         let output = Table::new(rows, widths)
             .header(header)
             .block(Block::bordered())
+            .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
             .highlight_symbol(">>");
-        output.render(layout[1], buf);
+        frame.render_stateful_widget(output, layout[1], &mut self.table_state);
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        if key_event.modifiers == KeyModifiers::CONTROL && key_event.code == KeyCode::Char('j') {
+            self.mode = Mode::Table;
+        } else if key_event.modifiers == KeyModifiers::CONTROL
+            && key_event.code == KeyCode::Char('j')
+        {
+            self.mode = Mode::Table;
+        } else if key_event.modifiers == KeyModifiers::CONTROL
+            && key_event.code == KeyCode::Char('k')
+        {
+            self.mode = Mode::Search;
+        } else if key_event.code == KeyCode::Esc {
+            self.exit();
+        }
+
+        match self.mode {
+            Mode::Search => match key_event.code {
+                KeyCode::Backspace => {
+                    self.search.pop();
+                    self.packages = self.pacman.search(&self.search);
+                }
+                // TODO: fix control
+                KeyCode::Char(value) => {
+                    self.search.push(value);
+                    self.packages = self.pacman.search(&self.search);
+                }
+                _ => {}
+            },
+            Mode::Table => match key_event.code {
+                KeyCode::Char('j') => self.next(),
+                KeyCode::Char('k') => self.previous(),
+                _ => {}
+            },
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.table_state.selected() {
+            Some(i) => {
+                if i >= self.packages.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.table_state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.table_state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.packages.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.table_state.select(Some(i));
+        // self.scroll_table_state = self.scroll_state.position(i * ITEM_HEIGHT);
     }
 }
--- src/tui.rs
+++ src/tui.rs
@@ -3,6 +3,7 @@ use std::io::{self, stdout, Stdout};
 use ratatui::{
     backend::CrosstermBackend,
     crossterm::{
+        event::{DisableMouseCapture, EnableMouseCapture},
         execute,
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     },
@@ -12,13 +13,13 @@ use ratatui::{
 pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
 pub fn init() -> io::Result<Tui> {
-    execute!(stdout(), EnterAlternateScreen)?;
+    execute!(stdout(), EnterAlternateScreen, EnableMouseCapture)?;
     enable_raw_mode()?;
     Terminal::new(CrosstermBackend::new(stdout()))
 }
 
 pub fn restore() -> io::Result<()> {
-    execute!(stdout(), LeaveAlternateScreen)?;
+    execute!(stdout(), LeaveAlternateScreen, DisableMouseCapture)?;
     disable_raw_mode()?;
     Ok(())
 }
